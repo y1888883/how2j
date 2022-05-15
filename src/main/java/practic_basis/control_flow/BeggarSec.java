414a6be2b4f67ec692e4ce69a73feecede0227c1
bamboo <y1888883@163.com>
2022-05-15 15:17:29 +0800

    20220515_how2j

---
+++ src/main/java/practic_basis/control_flow/BeggarSec.java
@@ -0,0 +1,17 @@
+package practic_basis.control_flow;
+
+/**
+ * @author: bamboo on 22/5/15
+ * @description: _
+ * 天朝有一个乞丐姓洪，去天桥要钱
+ * 第一天要了1块钱
+ * 第二天要了2块钱
+ * 第三天要了4块钱
+ * 第四天要了8块钱
+ * 以此类推
+ *
+ * 问题： 洪乞丐干10天，收入是多少？
+ */
+
+public class BeggarSec {
+}
